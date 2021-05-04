eea719886202404906aa8b0ded16810c0108c8cb
Aidan Lear <atl9004@g.rit.edu>
2021-05-03 19:20:50 -0500

    content on html is now styalized

--- speedrun/views.py
+++ speedrun/views.py
@@ -2,6 +2,14 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Run, User
 
 def home(request):
-    return HttpResponse("homepage for the our speedruns")
+    top_runs_list = Run.objects.order_by('time')[:]
+    top_runs_list.reverse()
+
+    context = {
+        'top_runs_list': top_runs_list,
+    }
+
+    return render(request, 'speedrun/home.html', context)
